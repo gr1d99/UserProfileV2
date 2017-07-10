92c64706a0337f237210ce40d7a368c134e56b5c
gr1d99 <gideonkimutai9@gmail.com>
2017-07-10 23:16:56 +0300

    made UserProfile class extend AbstractUser model

--- models.py
+++ models.py
@@ -1,5 +1,14 @@
 from __future__ import unicode_literals
 
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class UserProfile(AbstractUser):
+    # add your fields here
+
+    def __str__(self):
+        return 'User Profile for %(username)s' % dict(username=self.username)
+
+
+# for the fields to appear in django administration site you need to
+# go to admin.py file and add the fields you just added above
